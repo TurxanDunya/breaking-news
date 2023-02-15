082b718f32ea984e4f868bbfc098b2b8b0293643
Turxan Dunyamaliyev <turxan.dunyamaliyev@kapitalbank.az>
2023-02-15 21:04:34 +0400

    Entity class added based on database structure

---
+++ src/main/java/com/company/news/breakingnews/entity/News.java
@@ -0,0 +1,27 @@
+package com.company.news.breakingnews.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Data
+@Entity(name = "news")
+@Table(name = "news")
+public class News {
+
+    @Id
+    private Integer id;
+    private String head;
+    private String body;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "read_count")
+    private Integer readCount;
+
+}
